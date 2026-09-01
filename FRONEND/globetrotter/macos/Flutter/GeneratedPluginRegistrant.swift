1d7324dd834a4250fca4a8c9ae879bac4e7fe569
TUHEU <nadaljunior999@gmail.com>
2026-09-01 16:44:07 +0100

    feat: add emoji_picker_flutter plugin for emoji selection functionality

--- FRONEND/globetrotter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ FRONEND/globetrotter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import emoji_picker_flutter
 import file_picker_darwin
 import file_selector_macos
 import geolocator_apple
@@ -18,6 +19,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
